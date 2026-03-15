9d7e3f87eb61da84a1419340e2c0ec24d5215149
carlou-bit <66834422+bnzxs@users.noreply.github.com>
2026-03-15 22:29:46 +0800

    Update settings.py

--- backend/autoexpense/settings.py
+++ backend/autoexpense/settings.py
@@ -118,8 +118,8 @@ SIMPLE_JWT = {
 CORS_ALLOW_ALL_ORIGINS = True # Change for production
 
 # Celery Settings
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = os.environ.get('REDIS_URL') or os.environ.get('CELERY_BROKER_URL') or 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = CELERY_BROKER_URL
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
